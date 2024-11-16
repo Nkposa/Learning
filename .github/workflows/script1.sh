0a190ca651f25940c733f3149d5343744489050b
Nkposa <naveenhima91@gmail.com>
2024-11-16 16:40:04 +0530

    modified script

--- .github/workflows/script1.sh
+++ .github/workflows/script1.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 sudo apt-get install cowsay -y
-run cowsay -f dragon "i am the dragon...please" >> dragon.txt
+cowsay -f dragon "i am the dragon...please" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
 ls -ltr
+echo fuck
